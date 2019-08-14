2b52e10db346e9ac4414a7337c58c55c23c34baf
Jonathan Mousserion <quezako35@gmail.com>
2019-08-14 21:06:10 +0900

    base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'rules', loadChildren: './pages/rules/rules.module#RulesPageModule' },
+  { path: 'rule', loadChildren: './pages/rule/rule.module#RulePageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
